a3a576ad4aebd797630b03a5f4a434b364fd427d
kozimov786 <email@youremail.com>
2021-09-04 12:31:06 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    title: "Shangchi",
+    image: "./assets/images/shangchi.jpeg",
+    description: "A renowned criminal sired a child who he raised as his heir to an insidious secret society and named him Shang-Chi, the rising and advancing of the spirit.",
+    imageAlt: "shanchi movie poster",
+    url: "https://www.youtube.com/watch?v=giWIr7U1deA",
+  }
+})
